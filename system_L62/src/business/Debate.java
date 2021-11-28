32a4880208175334a5272ee29c3f0bb936608fb6
Usuario <Usuario@DESKTOP-8UG1D6B>
2021-11-28 17:05:18 +0100

    cambio

--- system_L62/src/business/Debate.java
+++ system_L62/src/business/Debate.java
@@ -1,27 +1,28 @@
 package business;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Debate {
-	public enum DebateState{ACTIVE,FINISHED}
+	public enum DebateState {
+		ACTIVE, FINISHED
+	}
+
 	private String id;
 	private String idArticulo;
 	private LocalDateTime fechaLimite;
 	private DebateState state;
-	
-	public Debate(String id,String idArticulo,LocalDateTime fechaLimite) {
-		if(fechaLimite.isBefore(LocalDateTime.now()))
-			state=DebateState.FINISHED;
+
+	public Debate(String id, String idArticulo, LocalDateTime fechaLimite) {
+		if (fechaLimite.isBefore(LocalDateTime.now()))
+			state = DebateState.FINISHED;
 		else
-			state=DebateState.ACTIVE;
-		this.id=id;
-		this.idArticulo=idArticulo;
-		this.fechaLimite=fechaLimite;
-		
+			state = DebateState.ACTIVE;
+		this.id = id;
+		this.idArticulo = idArticulo;
+		this.fechaLimite = fechaLimite;
+
 	}
-	
+
 	public String getId() {
 		return id;
 	}
@@ -34,8 +35,6 @@ public class Debate {
 		this.state = state;
 	}
 
-
-	
 	public String getIdArticulo() {
 		return idArticulo;
 	}
@@ -43,9 +42,9 @@ public class Debate {
 	public LocalDateTime getFechaLimite() {
 		return fechaLimite;
 	}
+
 	public void finalizarDebate() {
-		this.state=DebateState.FINISHED;
+		this.state = DebateState.FINISHED;
 	}
-	
-	
+
 }
